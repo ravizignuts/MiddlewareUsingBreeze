459f5c5d8a5521a8ab2df638732d8875180a3f70
RaviThakor <ravit@zignuts.com>
2023-01-30 17:11:16 +0530

    commit with controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,13 +28,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('admin',function(){
-    // if(auth()->user()->is_admin){ //You can do this but this is not right way beacuse you have to write this condition in all route
-    // dd('AdminPage');
-    // }else{
-    //     abort(401);
-    // }
-     dd('AdminPage');
+// Route::get('admin',function(){
+//     // if(auth()->user()->is_admin){ //You can do this but this is not right way beacuse you have to write this condition in all route
+//     // dd('AdminPage');
+//     // }else{
+//     //     abort(401);
+//     // }
+//      dd('AdminPage');
 
-})->middleware('admin');
+// })->middleware('auth','admin');
+
+// Route::group(['middleware'=>['auth','admin']],function(){
+//     Route::get('admin',function(){
+//         return view('auth.admin');
+//     });
+// });==>This is done by Controller also
+Route::get('admin',[AuthController::class,'index']);
 require __DIR__.'/auth.php';
